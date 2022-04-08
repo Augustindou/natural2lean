4501db775ab3a0d929f733336e80ef7fbc7fb9be
Augustin d'Oultremont <augustin.doultremont@hotmail.fr>
2022-04-08 15:28:26 +0200

    parity update

--- LeanUtils/Parity.lean
+++ LeanUtils/Parity.lean
@@ -3,8 +3,14 @@
 
 namespace Nat
   def even (a : Nat) : Prop := ∃ (n : Nat), a = 2 * n
+  def odd (a : Nat) : Prop := ¬ even a
+
+  def sqr (a : Nat) : Nat := a * a
 end Nat
 
 namespace Int
   def even (a : Int) : Prop := ∃ (n : Int), a = 2 * n
+  def odd (a : Int) : Prop := ¬ even a
+
+  def sqr (a : Int) : Int := a * a
 end Int
