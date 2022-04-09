ef1241e2bd49523b91a544ccedeb7e05683bb273
Augustin d'Oultremont <augustin.doultremont@hotmail.fr>
2022-04-09 18:40:17 +0200

    example 2

---
+++ LeanUtils/Div.lean
@@ -0,0 +1,14 @@
+/- Div : functions and theorems relative to the division and modulo operations -/
+
+namespace Nat
+  def divisible (b a : Nat) : Prop := a % b = 0
+
+  -- TODO : prove mod_3_poss... written as an axiom for now
+  axiom mod_rewrite {a b m : Nat} : (a % b = m) ↔ ∃ k, a = b * k + m
+  axiom mod_3_poss (a : Nat) : a % 3 = 0 ∨ a % 3 = 1 ∨ a % 3 = 2
+end Nat
+
+namespace Int
+  def divisible (b a : Int) : Prop := a % b = 0
+end Int
+
---
+++ tests/examples/example-2-sqr_q-mod-3/theorem.lean
@@ -0,0 +1,35 @@
+import Mathlib.Tactic.Ring
+import LeanUtils.Div
+
+open Nat
+
+theorem square_mod_3 (q : Nat) : (¬divisible 3 q) → (q^2 % 3 = 1) := by
+  intro H₁
+  have h_possibilities : q % 3 = 0 ∨ q % 3 = 1 ∨ q % 3 = 2 := mod_3_poss q
+
+  -- divide cases into 3 goals
+  cases h_possibilities with
+  | inl h0 => 
+    -- case 0
+    contradiction
+  | inr h1or2 => 
+    cases h1or2 with
+    | inl h1 => 
+      -- case 1
+      have q_rewrite : ∃k, q = 3 * k + 1 := mod_rewrite.mp h1
+      have ⟨k, hk⟩ := q_rewrite
+      have q_square : q^2 = 3 * (3 * k^2 + 2 * k) + 1  := by 
+        calc
+          q^2 = (3 * k + 1)^2 := by rw [hk]
+          _ = 9 * k^2 + 6 * k + 1 := by ring
+          _ = 3 * (3 * k^2 + 2 * k) + 1 := by ring
+      exact mod_rewrite.mpr ⟨3 * k^2 + 2 * k, by assumption⟩
+    | inr h2 => 
+      -- case 2
+      have q_rewrite : ∃k, q = 3 * k + 2 := mod_rewrite.mp h2
+      have ⟨k, hk⟩ := q_rewrite
+      have q_square : q^2 = 3 * (3 * k^2 + 4 * k + 1) + 1 := by 
+        calc
+          q^2 = (3 * k + 2)^2 := by rw [hk]
+          _ = 3 * (3 * k^2 + 4 * k + 1) + 1 := by ring
+      exact mod_rewrite.mpr ⟨3 * k^2 + 4 * k + 1, by assumption⟩
