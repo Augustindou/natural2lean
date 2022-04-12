57d3a81ff2c6dd8a2abed46b7fa0a48b48d79422
Augustindou <augustin.doultremontao@gmail.com>
2022-04-12 15:32:43 +0200

    rewriting 1st example to match the calc structure

--- Lean/examples/example-1-sqr_m-even/theorem.lean
+++ Lean/examples/example-1-sqr_m-even/theorem.lean
@@ -4,8 +4,12 @@ import LeanUtils.Parity
 open Nat
 
 theorem square_of_even_number_is_even (m : Nat) : (even m) → (even (m ^ 2)) := by
-  intro H₁
-  cases H₁ with 
-  | intro n hn =>
-    rw [hn]
-    exact ⟨2 * n ^ 2, by ring⟩
+  intro h₁
+  have ⟨n, h₂⟩ := h₁
+  have h₃ : m^2 = 2*(2*n^2) := by 
+    calc
+      m^2 = (2*n)^2 := by rw [h₂]
+      _ = 4*n^2 := by ring
+      _ = 2*(2*n^2) := by ring
+    
+  exact ⟨2 * n ^ 2, h₃⟩
