4a34c9aeb68b2588981d68b6ace8deac2a5d1329
Augustindou <augustin.doultremontao@gmail.com>
2022-04-13 15:47:46 +0200

    start for multiple identifiers

--- natural2lean/math_mode/__init__.py
+++ natural2lean/math_mode/__init__.py
@@ -2,10 +2,12 @@ from . import equation
 from . import expression
 from . import identifier
 from . import math
+from . import multiple_identifiers
 from . import translate_math
 
 from .equation import Equation
 from .expression import Expression
 from .identifier import Identifier
 from .math import Math
+from .multiple_identifiers import MultipleIdentifiers
 from .translate_math import Latex2LeanMath
