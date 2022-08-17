228910ca2d8e9aac830426986d3ca6f6372f78ba
Augustindou <augustin.doultremontao@gmail.com>
2022-08-17 22:22:00 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="natural2lean",
-    version="0.0.5",
+    version="0.0.6",
     author="Augustin d'Oultremont",
     author_email="augustin.doultremont@outlook.com",
     description="Translation of proofs from Natural Language to Lean",
